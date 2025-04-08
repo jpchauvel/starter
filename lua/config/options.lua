457805bad2b914c14c986dd152c7b3ef298c3fdb
Jean-Pierre Chauvel <jean.pierre.chauvel@autodesk.com>
2025-04-08 11:42:05 -0500

    adding options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.lazyvim_picker = "telescope"
+vim.lsp.inlay_hint.enable(true)
+vim.g.autoformat = false
+
+vim.g.lazyvim_python_lsp = "basedpyright"
+vim.diagnostic.config({ virtual_text = false })
+
+vim.g.editorconfig = true
