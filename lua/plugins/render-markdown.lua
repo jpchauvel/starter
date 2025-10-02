31075e360ed1de3cd2d4ecd193e162b38087f4c6
Jean-Pierre Chauvel <jean.p.chauvel@gmail.com>
2025-10-02 16:58:50 -0500

    adding render-markdown

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+    -- ft = { "markdown", "codecompanion" },
+    opts = function(_, opts)
+      return {}
+    end,
+  },
+}
