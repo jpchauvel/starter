92c8cf966d70d0e0e9f6a1355f2ba30fb24db78a
Jean-Pierre Chauvel <jean.p.chauvel@gmail.com>
2025-10-03 17:39:53 -0500

    resolving conflict in avante plugin

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -67,7 +67,7 @@ return {
     "MunifTanjim/nui.nvim",
     "ravitemer/mcphub.nvim",
     --- The below dependencies are optional,
-    "echasnovski/mini.pick", -- for file_selector provider mini.pick
+    "nvim-mini/mini.pick", -- for file_selector provider mini.pick
     "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
     "ibhagwan/fzf-lua", -- for file_selector provider fzf
